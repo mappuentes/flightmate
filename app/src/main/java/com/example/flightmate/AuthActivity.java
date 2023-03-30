370c79675bac3d58de48b353b19195790a1dca8b
Iván <ivan.molina.aebllan@alumnos.upm.es>
2023-03-30 02:34:30 +0200

    Funcionalidad de registro y login añadidas con gestión de errores.

--- app/src/main/java/com/example/flightmate/AuthActivity.java
+++ app/src/main/java/com/example/flightmate/AuthActivity.java
@@ -1,103 +1,112 @@
 package com.example.flightmate;
 
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-
-
-import android.content.Intent;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
-
-
-public class AuthActivity extends AppCompatActivity  {
-    Button btn_register;
-    EditText email, password;
-    FirebaseFirestore mFirestore;
-    FirebaseAuth mAuth;
+public class AuthActivity extends AppCompatActivity {
+    private EditText email,password;
+    private Button register, login;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.setTitle("¡Bienvenido a Flightmate!");
         setContentView(R.layout.activity_auth);
-        this.setTitle("Registro");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        mFirestore = FirebaseFirestore.getInstance();
-        mAuth = FirebaseAuth.getInstance();
-
         email = findViewById(R.id.editTextTextEmailAddress);
         password = findViewById(R.id.editTextTextPassword);
-        btn_register = findViewById(R.id.buttonRegister);
+        register = findViewById(R.id.buttonRegister);
+        login = findViewById(R.id.buttonLogin);
+        auth = FirebaseAuth.getInstance();
 
-        btn_register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String emailUser = email.getText().toString().trim();
-                String passUser = password.getText().toString().trim();
-
-                if ( emailUser.isEmpty() && passUser.isEmpty()){
-                    Toast.makeText(AuthActivity.this, "Complete los datos", Toast.LENGTH_SHORT).show();
-                }else{
-                    registerUser(emailUser, passUser);
-                }
-            }
-        });
-    }
-
-    private void registerUser(String emailUser, String passUser) {
-        mAuth.createUserWithEmailAndPassword(emailUser, passUser).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                String id = mAuth.getCurrentUser().getUid();
-                Map<String, Object> map = new HashMap<>();
-                map.put("id", id);
-                map.put("email", emailUser);
-                map.put("password", passUser);
-
-                mFirestore.collection("user").document(id).set(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+        register.setOnClickListener(
+                new View.OnClickListener() {
                     @Override
-                    public void onSuccess(Void unused) {
-                        finish();
-                        startActivity(new Intent(AuthActivity.this, HomeActivity.class));
-                        Toast.makeText(AuthActivity.this, "Usuario registrado con éxito", Toast.LENGTH_SHORT).show();
+                    public void onClick(View v) {
+                        String em = email.getText().toString();
+                        String pa = password.getText().toString();
+                        if(TextUtils.isEmpty(em)||TextUtils.isEmpty(pa))
+                        {
+                            Toast.makeText(AuthActivity.this,"Error: Password and email are required.",Toast.LENGTH_SHORT).show();
+                        }
+                        else if(pa.length() < 6)
+                        {
+                            Toast.makeText(AuthActivity.this,"Error: Password needs to be longer than 6 characters",Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            registerUser(em,pa);
+                        }
                     }
-                }).addOnFailureListener(new OnFailureListener() {
+                }
+        );
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = findViewById(R.id.editTextTextEmailAddress);
+                password = findViewById(R.id.editTextTextPassword);
+                String edmail = email.getText().toString().trim();
+                String edpassword = password.getText().toString().trim();
+                if(TextUtils.isEmpty(edmail)){
+                    Toast.makeText(AuthActivity.this,"Error: Email is required",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(edpassword)){
+                    Toast.makeText(AuthActivity.this,"Error: Password is required",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(password.length() < 6){
+                    Toast.makeText(AuthActivity.this,"Error: Password needs to be longer than 6 characters",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                //Authenticate the user.
+                auth.signInWithEmailAndPassword(edmail,edpassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(AuthActivity.this, "Error al guardar", Toast.LENGTH_SHORT).show();
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(AuthActivity.this, "Welcome to Flightmate!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        } else{
+                            Toast.makeText(AuthActivity.this, "Error " +task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
                     }
                 });
             }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(AuthActivity.this, "Error al registrar", Toast.LENGTH_SHORT).show();
-            }
         });
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        onBackPressed();
-        return false;
+    private void registerUser(String em, String pa) {
+        auth.createUserWithEmailAndPassword(em,pa).addOnCompleteListener(
+                AuthActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(AuthActivity.this,"Successful registration",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(AuthActivity.this, HomeActivity.class));
+                            finish();
+                        }
+                        else
+                        {
+                            Toast.makeText(AuthActivity.this,"Sign up failed",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
     }
-
-
 }

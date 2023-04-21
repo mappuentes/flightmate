bb5fc1c26d7c913a15d9c7090f4dbf2308772a8a
mappuentes <ma.puentes@alumnos.upm.es>
2023-04-21 14:21:03 +0200

    pequeños detalles de diseño.

--- app/src/main/java/com/example/flightmate/HomeActivity.java
+++ app/src/main/java/com/example/flightmate/HomeActivity.java
@@ -1,11 +1,16 @@
 package com.example.flightmate;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -13,8 +18,13 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Button backButton = (Button) findViewById(R.id.back_button);
         Button BuscarVueloButton = (Button) findViewById(R.id.buscar_button);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.txt_layout);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
 
         BuscarVueloButton.setOnClickListener(
                 new View.OnClickListener() {
@@ -23,14 +33,21 @@ public class HomeActivity extends AppCompatActivity {
                         startActivity(new Intent(HomeActivity.this, BuscarVuelosActivity.class));
                     }});
 
-        backButton.setOnClickListener(
-        new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(HomeActivity.this, AuthActivity.class));
-            }});
-
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu1,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }

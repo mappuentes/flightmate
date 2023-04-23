36bfc16955bb1f303807ce6eec267047695a3f8a
mappuentes <ma.puentes@alumnos.upm.es>
2023-04-23 17:55:26 +0200

    Creacion de desplegable para seleccionar entre 5 aerpuertos, de momento solo el aeropuerto de barajas y el prat.

--- app/src/main/java/com/example/flightmate/LlegadasActivity.java
+++ app/src/main/java/com/example/flightmate/LlegadasActivity.java
@@ -3,6 +3,10 @@ package com.example.flightmate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -24,6 +28,7 @@ import java.util.Date;
 import java.util.TimeZone;
 
 public class LlegadasActivity extends AppCompatActivity {
+    private String codAeropuerto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,75 +36,103 @@ public class LlegadasActivity extends AppCompatActivity {
         setContentView(R.layout.activity_llegadas);
         TableLayout tableLayout = findViewById(R.id.tableDepartures);
 
-        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Madrid"));
-        Date madridFecha = new Date();
+        AutoCompleteTextView autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
+        String[] options = {"Madrid-Barajas", "Barcelona-El Prat", "Opción 3", "Opción 4", "Opción 5"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, options);
+        autoCompleteTextView.setAdapter(adapter);
 
-        // Debuggear la fecha actual de Madrid.
-        // Toast.makeText(LlegadasActivity.this, "Fecha actual:"+ madridFecha, Toast.LENGTH_SHORT).show();
-        RequestQueue queue = Volley.newRequestQueue(LlegadasActivity.this);
-
-        String url ="https://airlabs.co/api/v9/schedules?arr_iata=MAD&api_key=e148d34f-4a9f-4ff5-bd1b-0b798d7bc488";
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onResponse(JSONObject response) {
-                Gson gson = new Gson();
-                JsonRequestResponse json_request_response = gson.fromJson(String.valueOf(response), JsonRequestResponse.class);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = (String) parent.getItemAtPosition(position);
+                setCodigoAeropuerto(selectedItem);
+                Toast.makeText(getApplicationContext(), "Has seleccionado: " + codAeropuerto, Toast.LENGTH_SHORT).show();
+                TimeZone.setDefault(TimeZone.getTimeZone("Europe/Madrid"));
+                Date madridFecha = new Date();
 
-                for (int i = 0; i < json_request_response.getResponse().size(); i++)
-                {
-                    String arr_time = json_request_response.getResponse().get(i).getArrTime();
-                    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-                    try {
-                        Date arr_time_date = format.parse(arr_time);
-                        if (arr_time_date.compareTo(madridFecha) > 0 ){
+                // Debuggear la fecha actual de Madrid.
+                // Toast.makeText(LlegadasActivity.this, "Fecha actual:"+ madridFecha, Toast.LENGTH_SHORT).show();
+                RequestQueue queue = Volley.newRequestQueue(LlegadasActivity.this);
 
-                            TableRow tableRow = new TableRow(LlegadasActivity.this);
+                String url ="https://airlabs.co/api/v9/schedules?arr_iata="+codAeropuerto+"&api_key=e148d34f-4a9f-4ff5-bd1b-0b798d7bc488";
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Gson gson = new Gson();
+                        JsonRequestResponse json_request_response = gson.fromJson(String.valueOf(response), JsonRequestResponse.class);
 
-                            SimpleDateFormat formatHourMin = new SimpleDateFormat("HH:mm");
-                            String arr_time_hour_min = formatHourMin.format(arr_time_date);
-                            TextView text_arr_time_hour_min = new TextView(LlegadasActivity.this);
-                            text_arr_time_hour_min.setText(arr_time_hour_min);
+                        for (int i = 0; i < json_request_response.getResponse().size(); i++)
+                        {
+                            String arr_time = json_request_response.getResponse().get(i).getArrTime();
+                            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+                            try {
+                                Date arr_time_date = format.parse(arr_time);
+                                if (arr_time_date.compareTo(madridFecha) > 0 ){
 
-                            TextView text_flight_iata = new TextView(LlegadasActivity.this);
-                            text_flight_iata.setText(json_request_response.getResponse().get(i).getFlightIata());
+                                    TableRow tableRow = new TableRow(LlegadasActivity.this);
 
-                            TextView text_DepIata = new TextView(LlegadasActivity.this);
-                            text_DepIata.setText(json_request_response.getResponse().get(i).getDepIata());
+                                    SimpleDateFormat formatHourMin = new SimpleDateFormat("HH:mm");
+                                    String arr_time_hour_min = formatHourMin.format(arr_time_date);
+                                    TextView text_arr_time_hour_min = new TextView(LlegadasActivity.this);
+                                    text_arr_time_hour_min.setText(arr_time_hour_min);
 
-                            TextView text_AirlineIata = new TextView(LlegadasActivity.this);
-                            text_AirlineIata.setText(json_request_response.getResponse().get(i).getAirlineIata());
+                                    TextView text_flight_iata = new TextView(LlegadasActivity.this);
+                                    text_flight_iata.setText(json_request_response.getResponse().get(i).getFlightIata());
 
-                            TextView text_AircraftIcao = new TextView(LlegadasActivity.this);
-                            text_AircraftIcao.setText(json_request_response.getResponse().get(i).getAircraftIcao());
+                                    TextView text_DepIata = new TextView(LlegadasActivity.this);
+                                    text_DepIata.setText(json_request_response.getResponse().get(i).getDepIata());
 
-                            TextView text_Status = new TextView(LlegadasActivity.this);
-                            text_Status.setText(json_request_response.getResponse().get(i).getStatus());
+                                    TextView text_AirlineIata = new TextView(LlegadasActivity.this);
+                                    text_AirlineIata.setText(json_request_response.getResponse().get(i).getAirlineIata());
 
-                            tableRow.addView(text_arr_time_hour_min);
-                            tableRow.addView(text_flight_iata);
-                            tableRow.addView(text_DepIata);
-                            tableRow.addView(text_AirlineIata);
-                            tableRow.addView(text_AircraftIcao);
-                            tableRow.addView(text_Status);
+                                    TextView text_AircraftIcao = new TextView(LlegadasActivity.this);
+                                    text_AircraftIcao.setText(json_request_response.getResponse().get(i).getAircraftIcao());
 
-                            tableLayout.addView(tableRow);
+                                    TextView text_Status = new TextView(LlegadasActivity.this);
+                                    text_Status.setText(json_request_response.getResponse().get(i).getStatus());
+
+                                    tableRow.addView(text_arr_time_hour_min);
+                                    tableRow.addView(text_flight_iata);
+                                    tableRow.addView(text_DepIata);
+                                    tableRow.addView(text_AirlineIata);
+                                    tableRow.addView(text_AircraftIcao);
+                                    tableRow.addView(text_Status);
+
+                                    tableLayout.addView(tableRow);
+                                }
+                            } catch (ParseException e) {
+                                throw new RuntimeException(e);
+                            }
+
                         }
-                    } catch (ParseException e) {
-                        throw new RuntimeException(e);
                     }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(LlegadasActivity.this, "error", Toast.LENGTH_SHORT).show();
 
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(LlegadasActivity.this, "error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                queue.add(request);
+
 
             }
         });
-        queue.add(request);
+
 
 
         //click listener
+    }
+
+    private void setCodigoAeropuerto(String selectecitem){
+        switch (selectecitem){
+            case ("Madrid-Barajas"):
+                this.codAeropuerto="MAD";
+                break;
+            case ("Barcelona-El Prat"):
+                this.codAeropuerto="BCN";
+                break;
+
+        }
     }
 }
